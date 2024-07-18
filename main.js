31dc0678ee083afbf750b355175f1c18599241d6
RacaMacaFoo <parasolka.ro@gmail.com>
2024-07-18 22:00:05 +0300

    all proj

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const firstNum = +prompt("Write your first number");
+const secondNum = +prompt("Write your second number");
+const thirdNum = +prompt("Write your third number");
+
+alert((firstNum + secondNum + thirdNum) / 3)
+
+
